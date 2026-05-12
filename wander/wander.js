59c30fd020a12b90f2e0f32ac2808e4766bc5409
kquinsland <kquinsland@users.noreply.github.com>
2026-05-12 18:52:51 +0000

    deploy: kquinsland/blog@c3fcd0796e913d0078d88948db41556df615d6e8

---
+++ wander/wander.js
@@ -0,0 +1,15 @@
+const wander = {
+  // Other Wander consoles that visitors can reach from this console.
+  consoles: [
+    'https://susam.net/wander/',
+  ],
+
+  // Initial recommendations for testing.
+  pages: [
+    'https://tools.karlquinsland.com/',
+    'https://tools.simonwillison.net/',
+  ],
+
+  // Websites and consoles this console should never load.
+  ignore: [],
+}
